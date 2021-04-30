e92c800011aa65b923d38bf74387e47ec75e0ad5
artm512 <ArataMakino@makinoshin-no-MacBook-Pro.local>
2021-04-30 17:42:37 +0900

    feat: axios 非同期通信demo

--- pages/index.vue
+++ pages/index.vue
@@ -3,15 +3,25 @@
     <p class="title">{{ message }}!</p>
     <hr>
     <router-link to="/price">Price Page</router-link>
+    <div>{{ users[0].id }}, {{ users[0].name }}</div>
   </div>
 </template>
 
 <script>
+const axios = require('axios');
+const url = 'https://jsonplaceholder.typicode.com/users';
+
 export default {
   data() {
     return {
       message: 'Hello World'
     }
+  },
+  asyncData({ params }) {
+    return axios.get(url)
+      .then(res => {
+        return { users: res.data };
+      })
   }
 }
 </script>
